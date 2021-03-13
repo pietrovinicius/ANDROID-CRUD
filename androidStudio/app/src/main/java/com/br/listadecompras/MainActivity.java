835dc31f07c449fb7abf92d955e5f2688ac27bd5
Pietro Lima <pietrovinicius@hotmail.com>
2021-03-13 18:36:34 -0300

    Criação do projeto

---
+++ androidStudio/app/src/main/java/com/br/listadecompras/MainActivity.java
@@ -0,0 +1,20 @@
+package com.br.listadecompras;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d("============Start: " , "App iniciado com sucesso!");
+
+
+
+    }
+}
